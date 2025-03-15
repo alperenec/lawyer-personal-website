1335dd5da2af25ba09cdaeff399c527880685f36
Alperen Emre Candan <91540339+alperenec@users.noreply.github.com>
2025-03-15 12:38:46 +0300

    latest point.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-    minify: true,
-    sourcemap: false,
-  },
 });
