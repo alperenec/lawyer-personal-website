f86f95f29227430ddc6a8157a0aacf7d4d6cd3e9
Alperen Emre Candan <91540339+alperenec@users.noreply.github.com>
2025-03-26 09:13:24 +0300

    added .env

--- api/index.js
+++ api/index.js
@@ -122,7 +122,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //added to client .env
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
 });
