0fdb4ad2055bc5423f294b7e031e175fd72d9a42
Alperen Emre Candan <91540339+alperenec@users.noreply.github.com>
2025-05-06 15:00:54 +0300

    restore index.js

--- api/index.js
+++ api/index.js
@@ -103,7 +103,8 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Add this near the end of your index.js file
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}!`);
 });
