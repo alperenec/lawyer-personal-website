b1b10badceddeec7e1dc3001a15693af5c782ff9
Alperen Emre Candan <91540339+alperenec@users.noreply.github.com>
2025-05-06 15:14:36 +0300

    fixed.

--- api/index.js
+++ api/index.js
@@ -104,6 +104,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
 });
